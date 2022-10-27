10d3f87f14a3677aa8f5e89cb0628d3d7c074d4c
TanvirHasanAshik <tanvirashik41@gmail.com>
2022-10-27 21:41:07 +0600

    project done. some feacher will added

--- js/phone.js
+++ js/phone.js
@@ -1,19 +1,30 @@
 /* Catch input value and search data */
 const searchPhoneData = () => {
     const searchField = document.getElementById('search-field');
+    const allPhone = document.getElementById('all-phone');
     const inputField = searchField.value.toLowerCase();
+    const details = document.getElementById('details');
+    allPhone.textContent = '';
+    details.textContent = '';
+    handleError(searchField.value);
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputField}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => loadData(data))
+    if (searchField.value != "") {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => loadData(data))
+    }
 
-    searchField.value = ''
+    searchField.value = '';
+
 };
-
+/* Show all data from API*/
 const loadData = (allData) => {
     const allPhone = document.getElementById('all-phone');
-    allPhone.textContent = '';
+    console.log(allData.status);
+    if (allData.status == false) {
+        document.getElementById('err-message').innerText = 'Search data is not available';
+    }
     if (!allData) {
         allPhone.innerHTML = `<p>Loading...</p>`
     } else {
@@ -27,10 +38,55 @@ const loadData = (allData) => {
                         <h5 class="card-title">Phone: ${phone.phone_name}</h5>
                         <p class="card-text">Brand: ${phone.brand}</p>
                     </div>
-                    <button onclick="phoneDetails()">Check Details</button>
+                    <button id="btn" class="btn btn-primary" onclick="detailsLoad('${phone.slug}')">Check Details</button>
                 </div>
                 `;
             allPhone.appendChild(div);
         });
     }
 }
+
+/* Show details from API*/
+const detailsLoad = (phoneInfo) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneInfo}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => phoneDetails(data));
+}
+
+const phoneDetails = (data) => {
+    const phoneData = data.data;
+    const details = document.getElementById('details');
+
+    details.innerHTML = `
+    <div class="card mb-3 mx-auto" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${phoneData.image}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${phoneData.name}</h5>
+                    <p class="card-text"><strong>Storage:</strong> ${phoneData.mainFeatures.storage}</p>
+                    <p class="card-text"><strong>Display:</strong> ${phoneData.mainFeatures.displaySize}</p>
+                    <p class="card-text"><strong>ChipSet:</strong> ${phoneData.mainFeatures.chipSet}</p>
+                    <p class="card-text"><strong>Memory: </strong>${phoneData.mainFeatures.memory}</p>
+                    <p class="card-text"><strong>ReleaseDate: </strong>${phoneData.releaseDate ? phoneData.releaseDate : 'No release date found'}</p>
+                    <p class="card-text"><strong>Sensor: </strong>${phoneData.mainFeatures.sensors}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+const handleError = (data) => {
+    try {
+        if (data == '') throw "Field must not be empty***";
+        if (data != '') throw document.getElementById('err-message').innerText = '';
+    } catch (err) {
+        document.getElementById('err-message').innerText = err;
+    }
+}
+
+
